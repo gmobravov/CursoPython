344beae320c6ad880073a1d6cacf7fa83a8249e9
gmobravov <gmo.bravo@gmail.com>
2020-01-31 12:37:35 -0300

    Add files via upload

---
+++ holamundo.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+
+"""
+n = 335;
+n
+print("Hola Mundo")
+
+nombre = input("Ingrese nombre :")
+print("Hola ", nombre)
+"""
+
+c = "{n} - {m} = {r}".format(m=2, r=3-2, n=3)
+ 
+print(c)
+
+c = "{:06d}".format(1234)
+ 
+print(c)
